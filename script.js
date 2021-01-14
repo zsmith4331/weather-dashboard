ce85e2fcbf1df5b9c17abfe2840826b9df9a2dfe
Zach Smith <zsmith4331@gmail.com>
2021-01-14 00:10:49 -0500

    Updated HTML & script files

--- script.js
+++ script.js
@@ -7,17 +7,47 @@ var APIKey ="ae877590736bec1c3ee178bfbe574f85"
 // URL needed to query the database //
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + APIKey;
 
-// AJAX call to the OpenWeatherMap API //
-$.ajax({
-    url: queryURL,
-    method: "GET"
+function currentWeather(city) {
+
+  // AJAX call to the OpenWeatherMap API //
+  $.ajax({
+  url: queryURL,
+  method: "GET"
   })
-    // Store all of the retrieved data inside of an object //
-    .then(function(response) {
+  // Store all of the retrieved data inside of an object //
+  .then(function(response) {
 
-      // Log the queryURL
-      console.log(queryURL);
+    console.log(response)
 
-      // Log the resulting object
-      console.log(response);
-    });
+    // Log the queryURL
+    console.log(queryURL);
+
+    // Log the resulting object
+    console.log(response);
+
+    console.log(response.name)
+
+    // console.log(response.icon)
+
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+  
+
+
+  });
+
+}
+
+
+
+// Event handler for user clicking the search button
+$("#select-city").on("click", function(event) {
+
+    // Prevent the button from trying to submit the form
+    event.preventDefault();
+
+    // Storing the city name
+    var inputCity = $("#city-input").val().trim();
+
+    // Running the searchBandsInTown function(passing in the artist as an argument)
+    currentWeather(inputCity);
+  });
